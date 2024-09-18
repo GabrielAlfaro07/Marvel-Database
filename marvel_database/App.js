fad17f1060e7a29a92af02bf420e65821d820ef9
GabrielAlfaro07 <galfaulate@gmail.com>
2024-09-18 00:21:56 -0600

    Added font family 'Marvel Regular'

--- marvel_database/App.js
+++ marvel_database/App.js
@@ -7,13 +7,17 @@ import {
   Button,
 } from "react-native";
 import CharacterCard from "./components/CharacterCard";
-import { fetchCharacters } from "./services/characterService"; // Import the service
+import { fetchCharacters } from "./services/characterService";
+// import Header from "./components/Header";
+import * as Font from "expo-font"; // Import expo-font to load custom font
+import { SafeAreaView } from "react-native";
 
 export default function App() {
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(true);
   const [offset, setOffset] = useState(0); // Dynamic offset state
   const limit = 20; // Static limit, you can change it as per need
+  const [fontLoaded, setFontLoaded] = useState(false); // Font loading state
 
   const getCharacters = async (offset, limit) => {
     setLoading(true);
@@ -22,11 +26,20 @@ export default function App() {
     setLoading(false);
   };
 
+  // Load the font when the app starts
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      MarvelRegular: require("./assets/fonts/MarvelRegular.ttf"), // Load custom font
+    });
+    setFontLoaded(true);
+  };
+
   useEffect(() => {
+    loadFonts(); // Load fonts on app start
     getCharacters(offset, limit); // Initial fetch
   }, [offset]);
 
-  if (loading) {
+  if (!fontLoaded || loading) {
     return (
       <View className="flex-1 justify-center items-center bg-gray-300">
         <ActivityIndicator size="large" color="#0000ff" />
@@ -35,16 +48,21 @@ export default function App() {
   }
 
   return (
-    <ScrollView className="bg-gray-300">
-      <View className="flex flex-wrap flex-row justify-around">
-        {characters.map((character) => (
-          <CharacterCard key={character.id} character={character} />
-        ))}
-      </View>
-      <View className="flex justify-center items-center mt-4">
-        {/* Button to load more characters by increasing the offset */}
-        <Button title="Load More" onPress={() => setOffset(offset + limit)} />
-      </View>
-    </ScrollView>
+    <SafeAreaView className="bg-gray-300 flex-1">
+      <ScrollView>
+        <Text className="text-xl" style={{ fontFamily: "MarvelRegular" }}>
+          MARVEL DATABASE
+        </Text>
+        <View className="flex flex-wrap flex-row justify-around">
+          {characters.map((character) => (
+            <CharacterCard key={character.id} character={character} />
+          ))}
+        </View>
+        <View className="flex justify-center items-center mt-4">
+          {/* Button to load more characters by increasing the offset */}
+          <Button title="Load More" onPress={() => setOffset(offset + limit)} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
