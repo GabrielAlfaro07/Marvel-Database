7533fa4b7fabc22305e921c5f59aeb1c5ba25073
GabrielAlfaro07 <galfaulate@gmail.com>
2024-09-19 02:32:22 -0600

    Added comic, event, series and story cards

--- marvel_database/components/SeriesCard.jsx
+++ marvel_database/components/SeriesCard.jsx
@@ -6,9 +6,9 @@ const screenWidth = Dimensions.get("window").width;
 const cardWidth = screenWidth / 2 - 24; // Dividing the screen width by 2 and accounting for margins
 
 // Fixed height for the card content to allow for up to three lines of text
-const cardHeight = 264; // Adjust this value as needed
+const cardHeight = 280; // Adjust this value as needed
 
-const CharacterCard = ({ character }) => {
+const SeriesCard = ({ series }) => {
   return (
     <View
       className="m-2 rounded-lg overflow-hidden bg-gray-800"
@@ -16,12 +16,12 @@ const CharacterCard = ({ character }) => {
     >
       <Image
         source={{
-          uri: `${character.thumbnail.path}.${character.thumbnail.extension}`,
+          uri: `${series.thumbnail.path}.${series.thumbnail.extension}`,
         }}
         style={{ width: cardWidth, height: 180 }}
         className="rounded-none"
       />
-      <View className="p-2 flex-1 justify-center items-center">
+      <View className="p-2 flex-1 justify-between">
         <Text
           style={{
             fontFamily: "MarvelRegular", // Use the loaded font
@@ -32,11 +32,11 @@ const CharacterCard = ({ character }) => {
           className="text-center text-2xl" // NativeWind classes
           numberOfLines={2}
         >
-          {character.name}
+          {series.title}
         </Text>
       </View>
     </View>
   );
 };
 
-export default CharacterCard;
+export default SeriesCard;
